liuchengxu/vista.vim

commit 5a4efb0244a75f1c9d947da90767e213f6d3641a

Switch line number and tag in finder rows (#231)

autoload/vista/finder.vim
@@ -86,7 +86,7 @@ function! s:GroupByKindForLSPData(lsp_items) abort
   for item in a:lsp_items
     let s:max_len_kind = max([s:max_len_kind, strwidth(item.kind)])
 
-    let lnum_and_text = printf('%s:%s', item.lnum, item.text)
+    let lnum_and_text = printf('%s:%s', item.text, item.lnum)
     let s:max_len_lnum_and_text = max([s:max_len_lnum_and_text, strwidth(lnum_and_text)])
 
     if has_key(s:grouped, item.kind)
@@ -102,14 +102,14 @@ function! s:FindColumnBoundary(grouped_data) abort
   for [kind, vals] in items(a:grouped_data)
     let s:max_len_kind = max([s:max_len_kind, strwidth(kind)])
 
-    let sub_max = max(map(copy(vals), 'strwidth(printf(''%s:%s'', v:val.lnum, v:val.text))'))
+    let sub_max = max(map(copy(vals), 'strwidth(printf(''%s:%s'', v:val.text, v:val.lnum))'))
     let s:max_len_lnum_and_text = max([s:max_len_lnum_and_text, sub_max])
   endfor
 endfunction
 
 function! s:IntoRow(icon, kind, item) abort
   let line = t:vista.source.line_trimmed(a:item.lnum)
-  let lnum_and_text = printf('%s:%s', a:item.lnum, a:item.text)
+  let lnum_and_text = printf('%s:%s', a:item.text, a:item.lnum)
   let row = printf('%s%s  [%s]%s  %s',
         \ lnum_and_text, repeat(' ', s:max_len_lnum_and_text- strwidth(lnum_and_text)),
         \ a:kind, repeat(' ', s:max_len_kind - strwidth(a:kind)),

autoload/vista/finder/fzf.vim
@@ -66,13 +66,13 @@ function! vista#finder#fzf#extract(line) abort
   " matching tag can't contain whitespace, but a tag does have a chance to contain whitespace?
 
   if g:vista#renderer#enable_icon
-    let items = matchlist(icon_lnum_tag, '\(.*\) \(\d\+\):\([a-zA-Z:#_.,<>]*\)')
-    let lnum = items[2]
-    let tag = items[3]
-  else
-    let items = matchlist(icon_lnum_tag, '\(\d\+\):\([a-zA-Z:#_.,<>]*\)')
-    let lnum = items[1]
+    let items = matchlist(icon_lnum_tag, '\(.*\) \([a-zA-Z:#_.,<>]*\):\(\d\+\)')
     let tag = items[2]
+    let lnum = items[3]
+  else
+    let items = matchlist(icon_lnum_tag, '\([a-zA-Z:#_.,<>]*\):\(\d\+\)')
+    let tag = items[1]
+    let lnum = items[2]
   end
 
   return [lnum, tag]
@@ -156,10 +156,10 @@ function! vista#finder#fzf#Highlight() abort
     let idx += 1
   endfor
 
-  execute 'syntax match FZFVistaNumber /\s*\zs\d*\ze:\w/' 'contains=FZFVistaIcon,'.join(icon_groups, ',')
-  execute 'syntax match FZFVistaTag    /^[^\[]*\(\[\)\@=/' 'contains=FZFVistaNumber,FZFVistaIcon,'.join(icon_groups, ',')
-  syntax match FZFVistaScope  /^[^]]*]/ contains=FZFVistaTag,FZFVistaBracket
-  syntax match FZFVista /^[^│┌└]*/ contains=FZFVistaBracket,FZFVistaNumber,FZFVistaTag,FZFVistaScope
+  execute 'syntax match FZFVistaTag    /\s*.*\(:\d\)\@=/' 'contains=FZFVistaIcon,'.join(icon_groups, ',')
+  execute 'syntax match FZFVistaNumber /^[^\[]*\(\[\)\@=/' 'contains=FZFVistaTag,FZFVistaIcon,'.join(icon_groups, ',')
+  syntax match FZFVistaScope  /^[^]]*]/ contains=FZFVistaNumber,FZFVistaBracket
+  syntax match FZFVista /^[^│┌└]*/ contains=FZFVistaBracket,FZFVistaTag,FZFVistaNumber,FZFVistaScope
   syntax match FZFVistaBracket /\[\|\]/ contained
 
   hi default link FZFVistaBracket  SpecialKey

commit f3b5bb856c2832acf2da0991ba6d8dd5bc1be842

Fix FZF Preview (no win32) (#251)
* Fix FZF Preview

This was broken since #231 when the object name and line number were
reversed. This PR uses grep to grab the line number only and pass it to
fzf preview.sh in the format it expects

* Exclude fix from win32

Close #248

autoload/vista/finder.vim
@@ -154,7 +154,16 @@ function! vista#finder#PrepareOpts(source, prompt) abort
 
   if exists('g:vista_fzf_preview')
     let preview_opts = call('fzf#vim#with_preview', g:vista_fzf_preview).options
-    let preview_opts[-1] = preview_opts[-1][0:-3] . t:vista.source.fpath . (g:vista#renderer#enable_icon ? ':{2}' : ':{1}')
+
+    if has('win32')
+      " keeping old code around since we are not sure if / how preview works on windows
+      let preview_opts[-1] = preview_opts[-1][0:-3] . t:vista.source.fpath . (g:vista#renderer#enable_icon ? ':{2}' : ':{1}')
+    else
+      let object_name_index = g:vista#renderer#enable_icon ? '2' : '1'
+      let extract_line_number = ':$(echo {' . object_name_index . "} | grep -o '[^:]*$')"
+      let preview_opts[-1] = preview_opts[-1][0:-3] . t:vista.source.fpath . extract_line_number
+    endif
+
     call extend(opts.options, preview_opts)
   endif
 